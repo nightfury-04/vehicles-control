36ed8561b637617cf346c27a55cf678c48fb891f
nightfury-04 <nightfury-04@users.noreply.github.com>
2022-09-29 00:40:10 -0500

    Fix validations

--- src/views/AddVehicle.jsx
+++ src/views/AddVehicle.jsx
@@ -3,14 +3,12 @@ import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import SelectImage from '../components/SelectImage'
 import { addVehicle } from '../actions/vehicles.actions'
-import { Container, Box, TextField, Button, IconButton, InputAdornment, Grid, Breadcrumbs, Link } from '@mui/material'
-import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { Container, Box, TextField, Button, Grid, Breadcrumbs, Link } from '@mui/material'
 
 const AddVehicle = () => {
     const dispatch = useDispatch()
     const [vehicleInfo, setVehicleInfo] = useState({})
     const history = useHistory()
-    const [visibility, setVisibility] = useState(false)
 
     const [, setImage, SelectImageComponent] = SelectImage({
         onChange: url => {
@@ -61,49 +59,62 @@ const AddVehicle = () => {
             >
                 Añadir un vehículo
             </h1>
-
-            <Grid container alignItems='flex-start' spacing={0}>
-                <Grid
-                    item
-                    xs={12}
-                    md={6}
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                >
-                    <SelectImageComponent />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField id='brand' name='brand' label='Marca' variant='outlined' fullWidth style={{ margin: '10px' }} onChange={handleChange} />
-                    <TextField id='model' name='model' label='Modelo' variant='outlined' fullWidth style={{ margin: '10px' }} onChange={handleChange} />
-                    <TextField
-                        id='year'
-                        name='year'
-                        label='Año'
-                        variant='outlined'
-                        fullWidth
-                        style={{ margin: '10px' }}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position='start'>
-                                    <IconButton onClick={() => {}} edge='end'>
-                                        {visibility ? <VisibilityOff /> : <Visibility /> && setVisibility(true)}
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
+            <form onSubmit={onSubmit}>
+                <Grid container alignItems='flex-start' spacing={0}>
+                    <Grid
+                        item
+                        xs={12}
+                        md={6}
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
                         }}
-                        onChange={handleChange}
-                    />
+                    >
+                        <SelectImageComponent />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            id='brand'
+                            name='brand'
+                            label='Marca'
+                            variant='outlined'
+                            type='text'
+                            fullWidth
+                            style={{ margin: '10px' }}
+                            required
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            id='model'
+                            name='model'
+                            label='Modelo'
+                            variant='outlined'
+                            type='text'
+                            fullWidth
+                            style={{ margin: '10px' }}
+                            required
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            id='year'
+                            name='year'
+                            label='Año'
+                            variant='outlined'
+                            type='number'
+                            fullWidth
+                            style={{ margin: '10px' }}
+                            required
+                            onChange={handleChange}
+                        />
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Box style={{ display: 'flex', justifyContent: 'right', marginTop: '20px' }}>
-                <Button variant='contained' style={{ height: '40px', width: 'auto' }} type='submit' form='add-vehicle'>
-                    Añadir vehículo
-                </Button>
-            </Box>
-            <form id='add-vehicle' onSubmit={onSubmit}></form>
+                <Box style={{ display: 'flex', justifyContent: 'right', marginTop: '20px' }}>
+                    <Button variant='contained' style={{ height: '40px', width: 'auto' }} type='submit'>
+                        Añadir vehículo
+                    </Button>
+                </Box>
+            </form>
         </Container>
     )
 }
